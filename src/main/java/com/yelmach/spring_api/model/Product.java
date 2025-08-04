44eb5fd0b5981d4814761d2381e6efe2dee16a0a
yelmach <yassineelmach0@gmail.com>
2025-08-04 14:16:11 +0100

    update product model

--- src/main/java/com/yelmach/spring_api/model/Product.java
+++ src/main/java/com/yelmach/spring_api/model/Product.java
@@ -3,21 +3,47 @@ package com.yelmach.spring_api.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Product")
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Document(collection = "products")
 public class Product {
 
     @Id
     private String id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100, message = " must be between 3 and 100 characters")
     private String name;
+
+    @Size(max = 500, message = "Description cannot axceed 500 characters")
     private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.01", message = "Price must be greater than 0")
+    @DecimalMax(value = "1_000_000.00", message = "Price cannot exceed 1_000_000.00")
     private double price;
 
-    public Product() {}
+    @NotBlank(message = "User ID is required")
+    private String userId;
+
+    public Product() {
+    }
 
     public Product(String name, String description, double price) {
         this.name = name;
         this.description = description;
         this.price = price;
+    }
+
+    public Product(String name, String description, double price, String userId) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.userId = userId;
     }
 
     public String getId() {
